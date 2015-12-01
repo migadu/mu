e32579513c0d31decb78a62591330be7dfdfcba6
Michael Bruderer <ojos@gmx.ch>
2015-12-01 18:41:04 +0100

    Added json format to cfind.

--- mu/mu-cmd-cfind.c
+++ mu/mu-cmd-cfind.c
@@ -157,52 +157,67 @@ leave:
 static void
 print_header (MuConfigFormat format)
 {
-	switch (format) {
-	case MU_CONFIG_FORMAT_BBDB:
-		g_print (";; -*-coding: utf-8-emacs;-*-\n"
-			 ";;; file-version: 6\n");
-		break;
-	case MU_CONFIG_FORMAT_MUTT_AB:
-		g_print ("Matching addresses in the mu database:\n");
-		break;
-	default:
-		break;
-	}
+        switch (format) {
+        case MU_CONFIG_FORMAT_BBDB:
+                g_print (";; -*-coding: utf-8-emacs;-*-\n"
+                         ";;; file-version: 6\n");
+                break;
+        case MU_CONFIG_FORMAT_MUTT_AB:
+                g_print ("Matching addresses in the mu database:\n");
+                break;
+        case MU_CONFIG_FORMAT_JSON:
+                g_print ("[");
+                break;
+        default:
+                break;
+        }
+}
+
+static void
+print_footer (MuConfigFormat format)
+{
+        switch (format) {
+        case MU_CONFIG_FORMAT_JSON:
+                g_print ("NULL]");
+                break;
+        default:
+                break;
+        }
 }
 
 static void
 each_contact_bbdb (const char *email, const char *name, time_t tstamp)
 {
-	char *fname, *lname, *now, *timestamp;
+        char *fname, *lname, *now, *timestamp;
 
-	fname	  = guess_first_name (name);
-	lname	  = guess_last_name (name);
-	now	  = mu_date_str ("%Y-%m-%d", time(NULL));
-	timestamp = mu_date_str ("%Y-%m-%d", tstamp);
+        fname     = guess_first_name (name);
+        lname     = guess_last_name (name);
+        now       = mu_date_str ("%Y-%m-%d", time(NULL));
+        timestamp = mu_date_str ("%Y-%m-%d", tstamp);
 
-	g_print ("[\"%s\" \"%s\" nil nil nil nil (\"%s\") "
-		 "((creation-date . \"%s\") (time-stamp . \"%s\")) nil]\n",
-		 fname, lname, email, now, timestamp);
+        g_print ("[\"%s\" \"%s\" nil nil nil nil (\"%s\") "
+                 "((creation-date . \"%s\") (time-stamp . \"%s\")) nil]\n",
+                 fname, lname, email, now, timestamp);
 
-	g_free (now);
-	g_free (timestamp);
-	g_free (fname);
-	g_free (lname);
+        g_free (now);
+        g_free (timestamp);
+        g_free (fname);
+        g_free (lname);
 }
 
 
 static void
 each_contact_mutt_alias (const char *email, const char *name)
 {
-	gchar *nick;
+        gchar *nick;
 
-	if (!name)
-		return;
+        if (!name)
+                return;
 
-	nick = guess_nick (name);
-	mu_util_print_encoded ("alias %s %s <%s>\n",
-			       nick, name, email);
-	g_free (nick);
+        nick = guess_nick (name);
+        mu_util_print_encoded ("alias %s %s <%s>\n",
+                               nick, name, email);
+        g_free (nick);
 
 }
 
@@ -294,124 +309,129 @@ typedef struct _ECData ECData;
 
 static void
 each_contact (const char *email, const char *name, gboolean personal,
-	      time_t tstamp, unsigned freq, ECData *ecdata)
+              time_t tstamp, unsigned freq, ECData *ecdata)
 {
-	if (ecdata->personal && !personal)
-		return;
+        if (ecdata->personal && !personal)
+                return;
 
-	if (tstamp < ecdata->after)
-		return;
+        if (tstamp < ecdata->after)
+                return;
 
-	switch (ecdata->format) {
-	case MU_CONFIG_FORMAT_MUTT_ALIAS:
-		each_contact_mutt_alias (email, name);
-		break;
-	case MU_CONFIG_FORMAT_MUTT_AB:
-		mu_util_print_encoded ("%s\t%s\t\n",
-				       email, name ? name : "");
-		break;
-	case MU_CONFIG_FORMAT_WL:
-		each_contact_wl (email, name);
-		break;
-	case MU_CONFIG_FORMAT_ORG_CONTACT:
-		each_contact_org_contact (email, name);
-		break;
-	case MU_CONFIG_FORMAT_BBDB:
-		each_contact_bbdb (email, name, tstamp);
-		break;
+        switch (ecdata->format) {
+        case MU_CONFIG_FORMAT_MUTT_ALIAS:
+                each_contact_mutt_alias (email, name);
+                break;
+        case MU_CONFIG_FORMAT_MUTT_AB:
+                mu_util_print_encoded ("%s\t%s\t\n",
+                                       email, name ? name : "");
+                break;
+        case MU_CONFIG_FORMAT_WL:
+                each_contact_wl (email, name);
+                break;
+        case MU_CONFIG_FORMAT_ORG_CONTACT:
+                each_contact_org_contact (email, name);
+                break;
+        case MU_CONFIG_FORMAT_BBDB:
+                each_contact_bbdb (email, name, tstamp);
+                break;
         case MU_CONFIG_FORMAT_CSV:
-		each_contact_csv (email, name);
-		break;
-	default:
-		print_plain (email, name, ecdata->color);
-	}
+                each_contact_csv (email, name);
+                break;
+  case MU_CONFIG_FORMAT_JSON:
+                each_contact_json (email, name);
+                break;
+        default:
+                print_plain (email, name, ecdata->color);
+        }
 }
 
 
 static MuError
 run_cmd_cfind (const char* pattern,
-	       gboolean personal, time_t after,
-	       MuConfigFormat format,
-	       gboolean color, GError **err)
+               gboolean personal, time_t after,
+               MuConfigFormat format,
+               gboolean color, GError **err)
 {
-	gboolean rv;
-	MuContacts *contacts;
-	size_t num;
-	ECData ecdata;
+        gboolean rv;
+        MuContacts *contacts;
+        size_t num;
+        ECData ecdata;
 
-	ecdata.personal = personal;
-	ecdata.after    = after;
-	ecdata.format	= format;
-	ecdata.color	= color;
+        ecdata.personal = personal;
+        ecdata.after    = after;
+        ecdata.format   = format;
+        ecdata.color    = color;
 
-	contacts = mu_contacts_new
-		(mu_runtime_path(MU_RUNTIME_PATH_CONTACTS));
-	if (!contacts) {
-		g_set_error (err, MU_ERROR_DOMAIN,
-			     MU_ERROR_CONTACTS_CANNOT_RETRIEVE,
-			     "could not retrieve contacts");
-		return MU_ERROR_CONTACTS_CANNOT_RETRIEVE;
-	}
+        contacts = mu_contacts_new
+                (mu_runtime_path(MU_RUNTIME_PATH_CONTACTS));
+        if (!contacts) {
+                g_set_error (err, MU_ERROR_DOMAIN,
+                             MU_ERROR_CONTACTS_CANNOT_RETRIEVE,
+                             "could not retrieve contacts");
+                return MU_ERROR_CONTACTS_CANNOT_RETRIEVE;
+        }
 
-	print_header (format);
-	rv = mu_contacts_foreach (contacts,
-				  (MuContactsForeachFunc)each_contact,
-				  &ecdata, pattern, &num);
-	mu_contacts_destroy (contacts);
+        print_header (format);
+        rv = mu_contacts_foreach (contacts,
+                                  (MuContactsForeachFunc)each_contact,
+                                  &ecdata, pattern, &num);
+        print_footer (format);
+        mu_contacts_destroy (contacts);
 
-	if (num == 0) {
-		g_warning ("no matching contacts found");
-		return MU_ERROR_NO_MATCHES;
-	}
+        if (num == 0) {
+                g_warning ("no matching contacts found");
+                return MU_ERROR_NO_MATCHES;
+        }
 
-	return rv ? MU_OK : MU_ERROR_CONTACTS;
+        return rv ? MU_OK : MU_ERROR_CONTACTS;
 }
 
 static gboolean
 cfind_params_valid (MuConfig *opts)
 {
-	switch (opts->format) {
-	case MU_CONFIG_FORMAT_PLAIN:
-	case MU_CONFIG_FORMAT_MUTT_ALIAS:
-	case MU_CONFIG_FORMAT_MUTT_AB:
-	case MU_CONFIG_FORMAT_WL:
-	case MU_CONFIG_FORMAT_BBDB:
-	case MU_CONFIG_FORMAT_CSV:
-	case MU_CONFIG_FORMAT_ORG_CONTACT:
-		break;
-	default:
-		g_warning ("invalid output format %s",
-			   opts->formatstr ? opts->formatstr : "<none>");
-		return FALSE;
-	}
+        switch (opts->format) {
+        case MU_CONFIG_FORMAT_PLAIN:
+        case MU_CONFIG_FORMAT_MUTT_ALIAS:
+        case MU_CONFIG_FORMAT_MUTT_AB:
+        case MU_CONFIG_FORMAT_WL:
+        case MU_CONFIG_FORMAT_BBDB:
+        case MU_CONFIG_FORMAT_CSV:
+        case MU_CONFIG_FORMAT_JSON:
+        case MU_CONFIG_FORMAT_ORG_CONTACT:
+                break;
+        default:
+                g_warning ("invalid output format %s",
+                           opts->formatstr ? opts->formatstr : "<none>");
+                return FALSE;
+        }
 
-	/* only one pattern allowed */
-	if (opts->params[1] && opts->params[2]) {
-		g_warning ("usage: mu cfind [options] [<ptrn>]");
-		return FALSE;
-	}
+        /* only one pattern allowed */
+        if (opts->params[1] && opts->params[2]) {
+                g_warning ("usage: mu cfind [options] [<ptrn>]");
+                return FALSE;
+        }
 
-	return TRUE;
+        return TRUE;
 }
 
 
 MuError
 mu_cmd_cfind (MuConfig *opts, GError **err)
 {
-	g_return_val_if_fail (opts, MU_ERROR_INTERNAL);
-	g_return_val_if_fail (opts->cmd == MU_CONFIG_CMD_CFIND,
-			      MU_ERROR_INTERNAL);
+        g_return_val_if_fail (opts, MU_ERROR_INTERNAL);
+        g_return_val_if_fail (opts->cmd == MU_CONFIG_CMD_CFIND,
+                              MU_ERROR_INTERNAL);
 
-	if (!cfind_params_valid (opts)) {
-		g_set_error (err, MU_ERROR_DOMAIN, MU_ERROR_IN_PARAMETERS,
-			     "invalid parameters");
-		return MU_ERROR_IN_PARAMETERS;
-	}
+        if (!cfind_params_valid (opts)) {
+                g_set_error (err, MU_ERROR_DOMAIN, MU_ERROR_IN_PARAMETERS,
+                             "invalid parameters");
+                return MU_ERROR_IN_PARAMETERS;
+        }
 
-	return run_cmd_cfind (opts->params[1],
-			      opts->personal,
-			      opts->after,
-			      opts->format,
-			      !opts->nocolor,
-			      err);
+        return run_cmd_cfind (opts->params[1],
+                              opts->personal,
+                              opts->after,
+                              opts->format,
+                              !opts->nocolor,
+                              err);
 }
